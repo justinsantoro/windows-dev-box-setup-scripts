11e9cd4fd68a62aeb42760ce04a5329eb841f698
justinsantoro <justin@grandjean.net>
2019-09-25 20:24:21 -0400

    add mvc component

--- scripts/VisualStudio.ps1
+++ scripts/VisualStudio.ps1
@@ -1,4 +1,4 @@
-choco install -y visualstudio2019community --package-parameters="'--add Microsoft.VisualStudio.Component.Git'"
+choco install -y visualstudio2019community --package-parameters="'--add Microsoft.VisualStudio.Component.Git --add Microsoft.VisualStudio.Web.Mvc4.ComponentGroup'"
 Update-SessionEnvironment #refreshing env due to Git install
 choco install -y visualstudio2019-workload-python
 choco install -y visualstudio2019-workload-manageddesktop
